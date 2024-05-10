9a7741e51ffb9e8c7db9dac08a576e77fe40dfa9
Mihir Patel <124758185+mihir1570@users.noreply.github.com>
2024-05-10 19:33:45 +0530

    first commit

---
+++ js/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8fd8b3324358ddbf.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
